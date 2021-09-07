7d4c6e627d44f0b6a4f7d3666968cd1eaec6b3f6
Amit-Torne <81459871+Amit-Torne@users.noreply.github.com>
2021-09-07 11:08:11 +0530

    Update header.h

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -1,1 +1,26 @@
+/**
+ * @file header.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#include <stdio.h>
+#ifndef __HEADER_H
+#define __HEADER_H
+
+void add_student();
+void find_rl();
+void find_fn();
+void find_c();
+void tot_s();
+void del_s();
+void up_s();
+
+
+
+#endif
