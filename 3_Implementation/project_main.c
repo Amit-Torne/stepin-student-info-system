ccdbc55c8389996d427e866662cea2ed272cc876
Amit-Torne <81459871+Amit-Torne@users.noreply.github.com>
2021-09-07 11:12:56 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -13,7 +13,33 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "add.c"
+
+int i = 0;
+struct sinfo {
+    char fname[50];
+    char lname[50];
+    int roll;
+    float cgpa;
+    int cid[10];
+} st[55];
+  
+void add_student()
+{
+    printf("Add the Students Details\n");
+    printf("Enter the first name of student\n");
+    scanf("%s", st[i].fname);
+    printf("Enter the last name of student\n");
+    scanf("%s", st[i].lname);
+    printf("Enter the Roll Number\n");
+    scanf("%d", &st[i].roll);
+    printf("Enter the CGPA you obtained\n");
+    scanf("%f", &st[i].cgpa);
+    printf("Enter the course ID of each course\n");
+    for (int j = 0; j < 5; j++) {
+        scanf("%d", &st[i].cid[j]);
+    }
+    i = i + 1;
+}
 
 void find_rl()
 {
